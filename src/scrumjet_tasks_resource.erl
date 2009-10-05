8f260ead339aab6589d955d15c73180673a3bdd7
Alain O'Dea <alain.odea@gmail.com>
2009-10-05 00:52:36 -0230

    Prototype Support for HTML Representation Paging
     - Honour range specified in request in HTML repr
    
     - Add pager previous/next links in HTML repr
    
     - Rename confusing range validator function
    
     - TODO: support range in URL query params

--- src/scrumjet_tasks_resource.erl
+++ src/scrumjet_tasks_resource.erl
@@ -30,32 +30,32 @@ init([]) -> {ok, #context{}}.
 malformed_request(ReqData, Context) ->
     try
         Range = range(wrq:get_req_header("Range", ReqData)),
-        valid_range(Range, ReqData, Context)
+        invalid_range(Range, ReqData, Context)
     catch
         _:_ -> {true, ReqData, Context}
     end.
 
-valid_range({Start, End}, ReqData, Context) when Start > End ->
-    {false, ReqData, Context};
-valid_range({Start, End}, ReqData, Context) when (End - Start) > 1000 ->
+invalid_range({Start, End}, ReqData, Context) when Start > End ->
+    {true, ReqData, Context};
+invalid_range({Start, End}, ReqData, Context) when (End - Start) > 1000 ->
     % reinterpret request as a valid smaller range
-    valid_range({Start, Start + 1000}, ReqData, Context);
-valid_range({Start, End}, ReqData, Context) ->
+    invalid_range({Start, Start + 1000}, ReqData, Context);
+invalid_range({Start, End}, ReqData, Context) ->
     Count = mnesia:table_info(scrumjet_task, size),
     ContentRange = lists:flatten(io_lib:format(
         "items ~w-~w/~w", [Start, End, Count])),
     NewReqData = wrq:set_resp_header("Content-Range", ContentRange, ReqData),
     {false, NewReqData, Context#context{range= {Start,End} }};
-valid_range(undefined, ReqData, Context) ->
+invalid_range(undefined, ReqData, Context) ->
     % default to 1000 items or whole data set
     Count = mnesia:table_info(scrumjet_task, size),
-    valid_range({0,lists:max([1000, Count])}, ReqData, Context).
+    invalid_range({0,lists:max([1000, Count])}, ReqData, Context).
 
 content_types_provided(ReqData, Context) ->
     {[{"text/html", to_html}, {"application/json", to_json}],
         ReqData, Context}.
 
-to_html(ReqData, Context) ->
+to_html(ReqData, Context=#context{range={Start, End}}) ->
     {[<<"<!DOCTYPE html>
 <html>
 <head>
@@ -66,19 +66,24 @@ html:head(),
 <body style='display:none'>
 <h1>ScrumJet Tasks</h1>
 <ul id='tasks'>">>,
-lists:foldl(fun html:li/2, [], tasks(0, 10)),
+lists:foldl(fun html:li/2, [], tasks(Start, End)),
 <<"
-</ul>
+</ul>">>,
+pager_links(Start, End),
+<<"
 </body>
 </html>
 ">>], ReqData, Context}.
 
-to_json(ReqData, Context=#context{range=undefined}) ->
-    to_json_handler({0, 1000}, ReqData, Context);
-to_json(ReqData, Context=#context{range={Start, End}}) ->
-    to_json_handler({Start, End}, ReqData, Context).
+pager_links(Start, End) ->
+    PageSize = End - Start,
+    [pager_link("Previous", Start - PageSize, End - PageSize),
+    pager_link("Next", Start + PageSize, End + PageSize)].
 
-to_json_handler({Start, End}, ReqData, Context) ->
+pager_link(Label, Start, End) ->
+    io_lib:format("<a href='./?start=~w&end=~w'>~s</a>", [Start, End, Label]).
+
+to_json(ReqData, Context=#context{range={Start, End}}) ->
     {mochijson2:encode([json:value(T) || T <- tasks(Start, End)]),
         ReqData, Context}.
 
